a8c93904c4b0e45781427340a99cbceae3587b3b
alberto03 <alberto.abis1@gmail.com>
2020-12-29 19:27:33 +0100

    modified user menu

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -41,14 +41,10 @@ AppAsset::register($this);
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
+        $menuItems[] = [
+            'label' => 'Logout ('.Yii::$app->user->identity->username.')',
+            'url' => ['/site/logout/']
+        ];
     }
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
