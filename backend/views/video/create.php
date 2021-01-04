55b9bd4eb54799b3e600433de5f4facabb0533f7
alberto03 <alberto.abis1@gmail.com>
2021-01-04 15:27:37 +0100

    added crud to video

---
+++ backend/views/video/create.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Video */
+
+$this->title = 'Create Video';
+$this->params['breadcrumbs'][] = ['label' => 'Videos', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="video-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <div>
+        <div class="upload-icon">
+            <i class="fas fa-upload"></i>
+        </div>
+
+        <p>Drag and drop a file you want to upload</p>
+
+        <p class="text-muted">Your video will be private until you publish it</p>
+    </div>
+
+
+</div>
